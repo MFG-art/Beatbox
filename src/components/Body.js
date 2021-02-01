9d67f230da8e68849cb4411847cb0bc56be2fb42
MFG-Art <fuentesmauricio75@gmail.com>
2021-02-01 14:29:19 -0600

    Created functioning play/pause button

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,34 @@
 import React from "react";
 
-let pause = true;
 let kick = new Audio("./audio/kick drum.wav");
+let snare = new Audio("./audio/snare drum.wav");
+let playPause = false;
+let tempo = 120;
+
+function Loop() {
+  let kickPat = [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0];
+  let snarePat = [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0];
+
+  let stepLength = 1000 * (15 / tempo);
+  let i = 0;
+
+  let step = setInterval(() => {
+    console.log(i);
+    if (kickPat[i] === 1) {
+      kick.currentTime = 0;
+      kick.play();
+    }
+
+    if (snarePat[i] === 1) {
+      snare.currentTime = 0;
+      snare.play();
+    }
+
+    i++;
+    i %= 16;
+    if (!playPause) clearInterval(step);
+  }, stepLength);
+}
 
 function Body() {
   return (
@@ -17,7 +44,17 @@ function Body() {
         </button>
         <button
           onClick={() => {
-            pause = !pause;
+            snare.play();
+          }}
+        >
+          Snare Drum
+        </button>
+        <button
+          onClick={() => {
+            playPause = !playPause;
+            if (playPause) {
+              Loop();
+            }
           }}
         >
           Play / Pause
@@ -26,4 +63,5 @@ function Body() {
     </div>
   );
 }
+
 export default Body;
